2d9427969b93dd23461c65bf9dbe2dd391a390f0
Pavel Kizhlo <pavelkizh@gmail.com>
2022-09-30 22:57:45 +0300

    feat: implement models for search item and search response

---
+++ youtube-client-app/src/app/shared/models/search-item.model.ts
@@ -0,0 +1,40 @@
+export interface ThumbnailSize {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface SearchItemModel {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: {
+    publishedAt: string;
+    channelId: string;
+    title: string;
+    description: string;
+    thumbnails: {
+      default: ThumbnailSize;
+      medium: ThumbnailSize;
+      high: ThumbnailSize;
+      standard: ThumbnailSize;
+      maxres: ThumbnailSize;
+    };
+    channelTitle: string;
+    tags: string[];
+    categoryId: string;
+    liveBroadcastContent: string;
+    localized: {
+      title: string;
+      description: string;
+    };
+    defaultAudioLanguage: string;
+  };
+  statistics: {
+    viewCount: string;
+    likeCount: string;
+    dislikeCount: string;
+    favoriteCount: string;
+    commentCount: string;
+  };
+}
