824ab8e79f6323d55903f1dd9ae2c7b33b8cf7c9
Pavel Kizhlo <pavelkizh@gmail.com>
2022-10-06 21:13:54 +0300

    feat: implement search item view

--- youtube-client-app/src/app/search/search-item/search-item.component.ts
+++ youtube-client-app/src/app/search/search-item/search-item.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { SearchItemModel } from '../../models/search-item.model';
+import { cardData } from '../../mocks/card-example';
 
 @Component({
   selector: 'app-search-item',
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
-export class SearchItemComponent {}
+export class SearchItemComponent {
+  searchItemData: SearchItemModel = cardData;
+}
--- youtube-client-app/src/app/shared/material.module.ts
+++ youtube-client-app/src/app/shared/material.module.ts
@@ -5,6 +5,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -13,6 +14,7 @@ const MaterialComponents = [
   MatToolbarModule,
   MatIconModule,
   MatMenuModule,
+  MatCardModule,
 ];
 
 @NgModule({
