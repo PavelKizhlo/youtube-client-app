b0c3951e853a90be90cf8922f06cb46dbb4ce9bf
Pavel Kizhlo <pavelkizh@gmail.com>
2022-10-06 18:15:17 +0300

    refactor: divide interface to smaller parts

--- youtube-client-app/src/app/models/search-item.model.ts
+++ youtube-client-app/src/app/models/search-item.model.ts
@@ -4,37 +4,46 @@ export interface ThumbnailSize {
   height: number;
 }
 
+export interface Thumbnails {
+  default: ThumbnailSize;
+  medium: ThumbnailSize;
+  high: ThumbnailSize;
+  standard: ThumbnailSize;
+  maxres: ThumbnailSize;
+}
+
+export interface Localization {
+  title: string;
+  description: string;
+}
+
+export interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: Localization;
+  defaultAudioLanguage: string;
+  defaultLanguage?: string;
+}
+
+export interface Statistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
+}
+
 export interface SearchItemModel {
   kind: string;
   etag: string;
   id: string;
-  snippet: {
-    publishedAt: string;
-    channelId: string;
-    title: string;
-    description: string;
-    thumbnails: {
-      default: ThumbnailSize;
-      medium: ThumbnailSize;
-      high: ThumbnailSize;
-      standard: ThumbnailSize;
-      maxres: ThumbnailSize;
-    };
-    channelTitle: string;
-    tags: string[];
-    categoryId: string;
-    liveBroadcastContent: string;
-    localized: {
-      title: string;
-      description: string;
-    };
-    defaultAudioLanguage: string;
-  };
-  statistics: {
-    viewCount: string;
-    likeCount: string;
-    dislikeCount: string;
-    favoriteCount: string;
-    commentCount: string;
-  };
+  snippet: Snippet;
+  statistics: Statistics;
 }
