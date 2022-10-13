830fe283f617872fd372cc1f54945473f342904e
Pavel Kizhlo <pavelkizh@gmail.com>
2022-10-13 18:40:11 +0300

    feat: create login-page, detailed-page, not-found-page components

---
+++ youtube-client-app/src/app/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+
+@NgModule({
+  declarations: [LoginPageComponent],
+  imports: [SharedModule],
+  exports: [LoginPageComponent],
+})
+export class AuthModule {}
---
+++ youtube-client-app/src/app/youtube/pages/detailed-page/detailed-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detailed-page',
+  templateUrl: './detailed-page.component.html',
+  styleUrls: ['./detailed-page.component.scss'],
+})
+export class DetailedPageComponent {}
