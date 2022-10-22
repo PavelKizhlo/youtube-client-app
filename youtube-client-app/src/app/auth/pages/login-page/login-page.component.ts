d20627cbb5e061a775c9fad4d8473ce8c5ef6600
Pavel Kizhlo <pavelkizh@gmail.com>
2022-10-22 16:43:56 +0300

    feat: implement login form validation using Reactive Forms

--- youtube-client-app/src/app/auth/pages/login-page/login-page.component.ts
+++ youtube-client-app/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { AuthService } from '../../services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { PasswordStrengthValidator } from '../../utils/password-strength.validator';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss'],
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  form: FormGroup;
+
+  email: FormControl;
+
+  password: FormControl;
+
   constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.email = new FormControl('', [Validators.required, Validators.email]);
+    this.password = new FormControl('', [Validators.required, PasswordStrengthValidator()]);
+
+    this.form = new FormGroup({
+      email: this.email,
+      password: this.password,
+    });
+  }
 
   submit() {
     if (!this.authService.isAuthenticated) {
--- youtube-client-app/src/app/shared/shared.module.ts
+++ youtube-client-app/src/app/shared/shared.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from './material.module';
 
 @NgModule({
-  imports: [CommonModule, MaterialModule, FormsModule],
-  exports: [CommonModule, MaterialModule, FormsModule],
+  imports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
+  exports: [CommonModule, MaterialModule, FormsModule, ReactiveFormsModule],
 })
 export class SharedModule {}
